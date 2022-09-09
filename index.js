cb20af5d4728a398404d991a6306b5dd6b7bf928
Furkan <muhammedcengiz1@gmail.com>
2022-09-09 21:39:23 +0300

    Inputtan gelen veriler array'de toplanıp butonun altında alt alta sıralacak şekilde ayarlandı ve gerekli style eklendi.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEL = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked from addEventListener")
+    const inputValue= inputEl.value
+    inputEl.value = ""
+    myLeads.push(inputValue);
+    renderLeads()
 })
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+
+        // listItems += "<li><a target='_blank' href='"+ myLeads[i]+"'>"+myLeads[i]+"</a>" + "</li>"
+        listItems +=
+        `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEL.innerHTML = listItems;
+}
+
